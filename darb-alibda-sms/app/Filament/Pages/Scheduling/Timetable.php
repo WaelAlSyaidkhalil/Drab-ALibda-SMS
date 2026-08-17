41e78f46e6657beb8e87894eed13ff893be2b2ba
Omar-da <omaraldalati3@gmail.com>
2026-08-17 13:52:18 +0300

    - rollback changes

--- darb-alibda-sms/app/Filament/Pages/Scheduling/Timetable.php
+++ darb-alibda-sms/app/Filament/Pages/Scheduling/Timetable.php
@@ -52,7 +52,7 @@ class Timetable extends Page implements HasForms
     public function publishStudentTimetable(): void
     {
         if (! $this->ensureContextSelected()) {
-            // $this->notifyMissingContext();
+            $this->notifyMissingContext();
             return;
         }
 
@@ -198,7 +198,7 @@ class Timetable extends Page implements HasForms
     public function openCell(string $day, int $slotId): void
     {
         if (! $this->ensureContextSelected()) {
-            // $this->notifyMissingContext();
+            $this->notifyMissingContext();
             return;
         }
 
@@ -278,18 +278,18 @@ class Timetable extends Page implements HasForms
         // Call the OR-Tools scheduling service to generate the timetable
         $success = app('App\Services\Admin\ORToolsSchedulerService')->generateTimetable();
 
-        // if ($success) {
-        //     Notification::make()
-        //         ->title(__('dashboard.labels.timetable_generated'))
-        //         ->success()
-        //         ->send();
+        if ($success) {
+            Notification::make()
+                ->title(__('dashboard.labels.timetable_generated'))
+                ->success()
+                ->send();
 
-        //     $this->loadGrid();
-        // } else {
-        //     Notification::make()
-        //         ->title(__('dashboard.labels.timetable_generation_failed'))
-        //         ->danger()
-        //         ->send();
-        // }
+            $this->loadGrid();
+        } else {
+            Notification::make()
+                ->title(__('dashboard.labels.timetable_generation_failed'))
+                ->danger()
+                ->send();
+        }
     }
 }
